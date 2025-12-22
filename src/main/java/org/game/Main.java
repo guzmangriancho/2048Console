6659357cadbe24971b2ac96c53849ab56d577747
Guzmán González de Riancho <guzmangriancho@gmail.com>
2025-12-22 13:19:27 +0100

    feat: main menu

--- src/main/java/org/game/Main.java
+++ src/main/java/org/game/Main.java
@@ -5,21 +5,43 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
+        HighScoreManager highScoreManager = new HighScoreManager();
+
+        while (true) {
+            System.out.println("\n          2048");
+            System.out.println("------------------------");
+            System.out.println("1.     Start Game");
+            System.out.println("2.  Show High Scores");
+            System.out.println("3.       Exit");
+
+            String option = scanner.next();
+
+            if (option.equals("1")) {
+                playGame(scanner, highScoreManager);
+            } else if (option.equals("2")) {
+                highScoreManager.show();
+            } else if (option.equals("3")) {
+                break;
+            }
+        }
+        scanner.close();
+    }
+
+    public static void playGame(Scanner scanner, HighScoreManager highScoreManager) throws Exception {
         Board board = new Board();
         Renderer renderer = new Renderer();
-        HighScoreManager highScoreManager = new HighScoreManager();
 
         while (true) {
             renderer.draw(board.getGrid(), board.getScore());
 
             if (board.isGameOver()) {
-                System.out.println("  GAME OVER! SCORE: " + board.getScore());
+                System.out.println("\n  GAME OVER! SCORE: " + board.getScore());
                 System.out.print("Enter your name: ");
                 String name = scanner.next();
 
                 highScoreManager.save(name, board.getScore());
                 highScoreManager.show();
-                break;
+                System.exit(0);
             }
 
             System.out.print("> ");
@@ -35,6 +57,5 @@ public class Main {
 
             board.addRandomNumber();
         }
-        scanner.close();
     }
 }
